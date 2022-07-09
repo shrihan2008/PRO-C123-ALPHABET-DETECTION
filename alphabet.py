a6fe949ff239f3f6b5c2a3ec3c811af9c43d8387
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-07-09 19:25:38 +0530

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from Pil import Image
+import PIL.ImageOps
+import os,ssl,time
+
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+X,y=fetch_openml('mnist_784',version=1,return_X_y=True)
+print(pd.Series(y).value_counts())
+
+classes=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+nclasses=len(classes)
+
+X =np.load('image.npz')['arr_0']
+y =pd.read_csv("labels.csv")["labels"]
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=2500,train_size=7500,random_state=49)
+X_train_scale=X_train/255.0
+X_test_scale=X_test/255.0
+
+clf=LogisticRegression(solver="saga",multiclass="multinomial").fit(X_train_scale,y_train)
+y_pred=clf.predict(X_test_scale)
+accuracy=accuracy_score(y_test,y_pred)
+print(accuracy)
+
+cap=cv2.videoCapture(0)
+
+while True:
+    try:
+        ret,frame=cap.read()
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        height,width=gray.shape
+        upper_left=(int((width/2-56),int(height/2-56)))
+        bottom_right=(int((width/2+56),int(height/2+56)))
+        cv2.rectangle(gray,upper_left,bottom_right,(0,255,0),2)
+        roi=gray[upper_left[1]:bottom_right[1],upper_left[0]:bottom_right[0]]
+        im_pil=Image.fromarray(roi)
+
+        image_bw=im_pil.convert('L')
+        image_bw_resized=image_bw.resize((28,28),Image.ANTIALIAS)
+        image_bw_resized_inverted=PIL.ImageOps.invert(image_bw_resized)
+        pxfilter=20
+        minpx=np.percentile(image_bw_resized_inverted,pxfilter)
+        image_bw_resized_inverted_scaled=np.clip(image_bw_resized_inverted-minpx,0,255)
+        maxpx=np.max(image_bw_resized_inverted)
+
+        image_bw_resized_inverted_scaled=np.asarray(image_bw_resized_inverted_scaled)/maxpx
+        test_sample=np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+        test_pred=clf.predict(test_sample)
+
+        print('predictedclasses',test_pred)
+
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+    
+    except Exception as e:
+        pass
+
+
+cap.release()
+cv2.destroyAllWindows()
